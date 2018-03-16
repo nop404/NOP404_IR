17020be18e2d0853c542daebad60e1ee6ab721bb
zhou <294442805@qq.com>
2018-03-16 18:00:15 +0800

    MCU_Tools增加按键输出功能
    增加按键输出功能

--- IR_MCU_V1.0/IR_MCU_V1.0.ino
+++ IR_MCU_V1.0/IR_MCU_V1.0.ino
@@ -22,6 +22,15 @@ String inputString = "";         // a String to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 IRsend irsend;
 void setup() {
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(A6, INPUT);
+  pinMode(A7, INPUT);
+
   // initialize serial:
   Serial.begin(115200);
   // reserve 200 bytes for the inputString:
@@ -29,26 +38,26 @@ void setup() {
 }
 
 
-  unsigned long  ircode_reverse(unsigned long src)
-    {
-        //printf("%08X\n",src);
-          Serial.println("----xxxxxxxxx----");
-        Serial.print(src,HEX);
-        src=((src>>1)&0x55555555)|((src&0x55555555)<<1);
-        src=((src>>2)&0x33333333)|((src&0x33333333)<<2);
-        src=((src>>4)&0x0F0F0F0F)|((src&0x0F0F0F0F)<<4);
-        src=((src>>8)&0x00FF00FF)|((src&0x00FF00FF)<<8);
-        src=(src>>16)|(src<<16);
-       
-        src= (((src & 0xff000000) >> 24) | ((src & 0x00ff0000) >> 8) | ((src & 0x0000ff00) << 8) | ((src & 0x000000ff) << 24));
-        Serial.println("------------");
-        Serial.print(src,HEX);
-        src=(src<<8)+(0xFF-(src&0xFF));
-         Serial.println("........");
-        Serial.print(src,HEX);
-        return src;
-    }
-    
+unsigned long  ircode_reverse(unsigned long src)
+{
+  //printf("%08X\n",src);
+  Serial.println("----xxxxxxxxx----");
+  Serial.print(src, HEX);
+  src = ((src >> 1) & 0x55555555) | ((src & 0x55555555) << 1);
+  src = ((src >> 2) & 0x33333333) | ((src & 0x33333333) << 2);
+  src = ((src >> 4) & 0x0F0F0F0F) | ((src & 0x0F0F0F0F) << 4);
+  src = ((src >> 8) & 0x00FF00FF) | ((src & 0x00FF00FF) << 8);
+  src = (src >> 16) | (src << 16);
+
+  src = (((src & 0xff000000) >> 24) | ((src & 0x00ff0000) >> 8) | ((src & 0x0000ff00) << 8) | ((src & 0x000000ff) << 24));
+  Serial.println("------------");
+  Serial.print(src, HEX);
+  src = (src << 8) + (0xFF - (src & 0xFF));
+  Serial.println("........");
+  Serial.print(src, HEX);
+  return src;
+}
+
 void loop() {
   unsigned char i;
   unsigned char datalen;
@@ -56,43 +65,103 @@ void loop() {
   unsigned long  temp;
   // print the string when a newline arrives:
   if (stringComplete) {
- 
-   // Serial.println(inputString);
-
-  //  String2Hex((&inputString);
-    // clear the string:
-    //Serial.println(inputString.length());
-    datalen=inputString.length()-1;
-   Serial.println(datalen);
-    for(i=0;i<datalen;i++)
+    if (inputString.length() == 2)
     {
-     if(inputString[i]>='a')
-     {
-     temp=(inputString[i]-'a'+10);
-        Serial.println(temp);
-        remotecode+=(temp<<((datalen-1-i)*4));
-      }else if(inputString[i]>='A')
-      {
-       temp=(inputString[i]-'A'+10);
-        Serial.println(temp);
-        remotecode+=(temp<<((datalen-1-i)*4));
-        }
-       else
-       {
-       temp=(inputString[i]-'0');
-       remotecode+=(temp<<((datalen-1-i)*4));
-        }
-    }
+      Serial.println(inputString);
+      pinMode(A0, INPUT);
+      pinMode(A1, INPUT);
+      pinMode(A2, INPUT);
+      pinMode(A3, INPUT);
+      pinMode(A4, INPUT);
+      pinMode(A5, INPUT);
+      pinMode(A6, INPUT);
+      pinMode(A7, INPUT);
+      switch (inputString[0]) {
+        case '0':
+          pinMode(A6, OUTPUT);
+          digitalWrite(A6, LOW);
+          Serial.println("K0");
+          break;
+        case '1':
+          pinMode(A5, OUTPUT);
+          digitalWrite(A5, LOW);
+          Serial.println("K1");
+          break;
 
-   //remotecode=0x00FD00FF;
-   //Serial.println(remotecode);
- 
-   
-   irsend.sendNEC(ircode_reverse(remotecode),32);
-    remotecode=0;
-    inputString = "";
-   
-    stringComplete = false;
+        case '2':
+          pinMode(A4, OUTPUT);
+          digitalWrite(A4, LOW);
+          Serial.println("K2");
+          break;
+        case '3':
+          pinMode(A3, OUTPUT);
+          digitalWrite(A3, LOW);
+          Serial.println("K3");
+          break;
+        case '4':
+          pinMode(A2, OUTPUT);
+          digitalWrite(A2, LOW);
+          Serial.println("K4");
+          break;
+        case '5':
+          pinMode(A1, OUTPUT);
+          digitalWrite(A1, LOW);
+          Serial.println("K5");
+          break;
+        case '6':
+          pinMode(A0, OUTPUT);
+          digitalWrite(A0, LOW);
+          Serial.println("K6");
+          break;
+        case '7':
+          pinMode(A7, OUTPUT);
+          digitalWrite(A7, LOW);
+          Serial.println("NULL");
+          break;
+        default:
+          pinMode(A0, INPUT);
+          pinMode(A1, INPUT);
+          pinMode(A2, INPUT);
+          pinMode(A3, INPUT);
+          pinMode(A4, INPUT);
+          pinMode(A5, INPUT);
+          pinMode(A6, INPUT);
+          pinMode(A7, INPUT);
+          break;
+      }
+      inputString = "";
+      stringComplete = false;
+    }
+    else if (inputString.length()>2)
+    {
+      datalen = inputString.length() - 1;
+      Serial.println(datalen);
+      for (i = 0; i < datalen; i++)
+      {
+        if (inputString[i] >= 'a')
+        {
+          temp = (inputString[i] - 'a' + 10);
+          Serial.println(temp);
+          remotecode += (temp << ((datalen - 1 - i) * 4));
+        } else if (inputString[i] >= 'A')
+        {
+          temp = (inputString[i] - 'A' + 10);
+          Serial.println(temp);
+          remotecode += (temp << ((datalen - 1 - i) * 4));
+        }
+        else
+        {
+          temp = (inputString[i] - '0');
+          remotecode += (temp << ((datalen - 1 - i) * 4));
+        }
+      }
+
+      irsend.sendNEC(ircode_reverse(remotecode), 32);
+      remotecode = 0;
+      inputString = "";
+
+      stringComplete = false;
+    }
   }
 }
 
@@ -102,7 +171,7 @@ void loop() {
   delay response. Multiple bytes of data may be available.
 */
 
- 
+
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
